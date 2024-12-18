ac53eb93f6d50b974549231db3b0a4525c8b21e8
Daniehen <119754452+Daniehen@users.noreply.github.com>
2024-12-18 14:21:20 +0100

    fixed the code to read the path properly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
-    with open(books/frankenstein.txt) as f:
+    with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+    print(file_contents)
     return 0
 
 main()
